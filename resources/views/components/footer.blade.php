b6ccf1c0470974e3dc42bd2541019d2c2c0b8456
Ringle <ringunger@gmail.com>
2021-07-15 15:35:32 +0300

    Initial Commit after full setup

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<style>
+
+</style>
+<footer>
+    <div class="footer-content boxed">
+
+    </div>
+</footer>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.web')
+
+@section('content')
+    <style>
+
+    </style>
+    <div class="slash" >
+        <div class="row boxed">
+            <div class="col-12 col-sm-8 col-xl-9">
+                <h6 class="text-black-50">ARE YOU STUCK SOMEWHERE?</h6>
+                <h3>Solve Big Challenges by Power of the People</h3>
+                <h4 class=""><i>Transparent, Trustable, Reliable</i></h4>
+                <p></p>
+            </div>
+            <div class="col-12 col-sm-4 col-xl-3">
+                <div class="action">
+                    <div class="">
+                        <i class="la la-arrow-circle-left"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <!-- Page Heading -->
+        <h1 class="my-4">Page Heading
+            <small>Secondary Text</small>
+        </h1>
+
+        <!-- Project One -->
+        <div class="row">
+            <div class="col-md-5">
+                <a href="#">
+                    <img class="img-fluid rounded mb-3 mb-md-0" src="https://picsum.photos/700/300" alt="">
+                </a>
+            </div>
+            <div class="col-md-7">
+                <h3>Project One</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+                <a class="btn btn-outline-custom" href="#">View Project</a>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+    </div>
+
+
+
+
+
+@endsection
+@section('script')
+
+@endsection
